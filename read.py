753fcf98ba6871ff0dad1e80f0ef0baf18205685
Marcio Andrade <marcioads01@gmail.com>
2020-03-28 16:57:21 -0300

    refatoção do codigo e inclusao de metodo para atulização da chave de versao do version.json

--- read.py
+++ read.py
@@ -1,30 +1,25 @@
+#coding = utf-8
 import json
-import re
 
 def ler_json():
     with open(caminho_arquivo ,'r',encoding='utf8') as f:
         return json.load(f)
+
 # leitura do arquivo versions.json
 caminho_arquivo = 'versions.json'
-data1 = ler_json()
-versao1 = (data1['registration'])
-#print(data1)
-versao1 = re.sub('[^0-9]', '', str(versao1))
-print("versao arq1 = " + versao1)
+version_file = ler_json()
 
 # leitura do arquivo package.json
 caminho_arquivo = 'package.json'
-data2 = ler_json()
-versao2 = (data2['version'])
-#print(data2)
-versao2 = re.sub('[^0-9]', '', str(versao2))
-print("versao arq2 = " + versao2)
+package_file = ler_json()
+package_version = (package_file['version'])
+package_version = package_version.split('.')
 
-# realizando o procedimento de comparação entre as versoes dos documentos
-if versao1 == versao2:
-    print("os arquivos tem mesma versao")
-elif versao1 != versao2:
-    print("os arquivos tem versao diferente")
-
-    
-
+if str(version_file['registration'][2]) == package_version[2]:
+    print('Versoes iguais.')
+else:
+    version_file['registration'][2] = int(package_version[2])
+    arquivo_version = open('versions.json', 'w', encoding='utf8')
+    json.dump(version_file, arquivo_version)
+    arquivo_version.close()
+    print('Versao atualizada.')
